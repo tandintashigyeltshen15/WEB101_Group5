ce2889eb2fba5d795b3afa7fccd5b0f360b0f0f6
tandintashigyeltshen15 <gyeltshentandint439@gmail.com>
2026-05-27 14:37:13 +0600

    commit

--- fluxtube-frontend/fluxtube-frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["img.youtube.com", "images.unsplash.com", "i.ytimg.com"],
-  },
-};
-
-export default nextConfig;
